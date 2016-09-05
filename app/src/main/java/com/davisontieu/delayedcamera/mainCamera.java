37a656cd911aeb61b9f34260309405d3fec397f6
Davison <dkhac.tieu@gmail.com>
2016-09-04 23:24:20 -0400

    Initial commit.

---
+++ app/src/main/java/com/davisontieu/delayedcamera/mainCamera.java
@@ -0,0 +1,15 @@
+package com.davisontieu.delayedcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_camera);
+    }
+
+
+}
